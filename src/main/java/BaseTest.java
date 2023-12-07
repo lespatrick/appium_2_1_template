c31e4cb1849efa736bd5d071e3becf2b5e9ba270
Leszek Barszcz <leszek.barszcz@brightinventions.pl>
2023-12-07 13:19:13 +0100

    Basic configuration

---
+++ src/main/java/BaseTest.java
@@ -0,0 +1,30 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.Before;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+    public IOSDriver driver;
+
+    @Before
+    public void init() throws MalformedURLException {
+        XCUITestOptions options = new XCUITestOptions()
+                // Device name (from simulator)
+                .setDeviceName("iPhone 11")
+                // iOS version
+                .setPlatformVersion("16.4")
+                // simulator UDID, use command
+                // xcrun simctl list devices | grep "iPhone 11"
+                // for finding the UDID
+                .setUdid("simulator UDID")
+                .setLocale("IL")
+                .setApp("path/to/file.app");
+        driver = new IOSDriver(
+                // The default URL in Appium is http://127.0.0.1:4723/wd/hub
+                new URL("http://127.0.0.1:4723"), options
+        );
+    }
+}
